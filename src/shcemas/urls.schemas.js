d72e2ffc8a055107fe0fa1bd36a7a3463b881716
Joaofsrs <rsjoao12@gmail.com>
2023-05-22 13:11:58 -0300

    correcao envio urls/shorten

--- src/shcemas/urls.schemas.js
+++ src/shcemas/urls.schemas.js
@@ -1,5 +1,5 @@
 import joi from "joi";
 
 export const urlSchema = joi.object({
-    url: joi.string().uri({ scheme: ['https'] }).required()
+    url: joi.string().uri({ scheme: ['https', 'http'] }).required()
 });
